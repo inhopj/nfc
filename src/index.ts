ee6475054ed9863f4231c5001e9f19cf334937ee
inhopj <fra.cirone@gmail.com>
2022-06-23 17:28:11 +0200

    add TODOs in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-// TODO - check if it can be removed and only importuseState and useEffect, test build!!!
+/**
+ * TODOs - Add eslint, add husky pre-commit hook
+ */
+// TODO - check if it can be removed and only import useState and useEffect, test build!!!
 // import { useState, useEffect } from 'react';
 import * as React from 'react';
 
