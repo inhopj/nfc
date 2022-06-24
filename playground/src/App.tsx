2c63e34a0f68b6d47d3e4985689856f7c61ce636
inhopj <fra.cirone@gmail.com>
2022-06-24 11:44:10 +0200

    import and use nfc dependency into App.tsx

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from 'react'
+import { useCounter } from 'nfc'
+
 
 function App() {
+  const counter = useCounter()
   return (
     <div className="App">
       FUCK
+      {counter}
     </div>
   );
 }
