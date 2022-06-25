7187d728b3b76768b92260fb61fef89f2107af69
inhopj <fra.cirone@gmail.com>
2022-06-25 11:23:38 +0200

    add some tailwind classes just to test

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -5,7 +5,9 @@ import { useCounter } from 'nfc'
 function App() {
   const counter = useCounter()
   return (
-    <div className="App">
+    <div
+      className='flex flex-col min-h-screen justify-start items-center p-4 md:p-8 bg-zinc-900 text-white'
+    >
       FUCK
       {counter}
     </div>
