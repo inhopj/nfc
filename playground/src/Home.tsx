4449e9013b29c65899d854d242efad885f15fe7e
inhopj <fra.cirone@gmail.com>
2022-06-26 19:31:33 +0200

    add some content in Home page

--- playground/src/Home.tsx
+++ playground/src/Home.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <>
-      CIAO
-    </>
+    <div
+      className='flex flex-col min-h-screen justify-start items-center p-4 md:p-8 bg-zinc-900 text-white'
+    >
+      WELCOME TO THE NFC PLAYGROUND
+    </div>
   );
 }
 
