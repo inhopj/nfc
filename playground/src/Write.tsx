0271925b9e5d94767f78e83e1aaacbf7cc1cf151
inhopj <fra.cirone@gmail.com>
2022-06-26 19:31:47 +0200

    same for Write page

--- playground/src/Write.tsx
+++ playground/src/Write.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Write = () => {
 
   return (
-    <>
-      <main>
-        <h2>Write Page</h2>
-      </main>
-    </>
+    <div
+      className='flex flex-col min-h-screen justify-start items-center p-4 md:p-8 bg-zinc-900 text-white'
+    >
+      WRITE PAGE
+    </div>
   );
 }
 
