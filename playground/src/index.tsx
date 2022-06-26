510a31d88d56de90b0a3d567b07a6634a47cf9fb
inhopj <fra.cirone@gmail.com>
2022-06-26 20:02:32 +0200

    use HashRouter instead of BrowserRouter

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
