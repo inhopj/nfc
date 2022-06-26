a0c29313017e625f7dadf8ab3e9eeb750101c244
inhopj <fra.cirone@gmail.com>
2022-06-26 19:31:09 +0200

    naming from Read to Scan

---
+++ playground/src/Scan.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scan = () => {
+  return (
+    <div
+      className='flex flex-col min-h-screen justify-start items-center p-4 md:p-8 bg-zinc-900 text-white'
+    >
+      SCAN PAGE
+    </div>
+  );
+}
+
+export default Scan
