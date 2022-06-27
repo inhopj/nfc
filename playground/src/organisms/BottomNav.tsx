272fedda8d8b8e6d2f9fe027a16f39aa253c4b17
inhopj <fra.cirone@gmail.com>
2022-06-27 06:54:02 +0200

    sync route and bottom nav element focus

--- playground/src/organisms/BottomNav.tsx
+++ playground/src/organisms/BottomNav.tsx
@@ -4,15 +4,18 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
 import NfcIcon from '@mui/icons-material/Nfc';
 import CreateIcon from '@mui/icons-material/Create';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 const BottomNav = () => {
   const [value, setValue] = useState()
+  const pathname = useLocation().pathname
+  console.log(location);
+  
   return (
     <BottomNavigation
       showLabels
-      value={value}
+      value={pathname}
       onChange={(event, newValue) => {
         setValue(newValue);
       }}
@@ -22,18 +25,21 @@ const BottomNav = () => {
         icon={<HomeIcon />}
         component={Link}
         to="/"
-      />
+        value='/'
+        />
       <BottomNavigationAction
         label="Scan"
         icon={<NfcIcon />}
         component={Link}
         to="/scan"
-      />
+        value='/scan'
+        />
       <BottomNavigationAction
         label="Write"
         icon={<CreateIcon />}
         component={Link}
         to="/write"
+        value='/write'
         />
     </BottomNavigation>
   );
