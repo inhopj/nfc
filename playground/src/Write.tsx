091a59b0dec2dbb396765448f310ef405c54090f
inhopj <fra.cirone@gmail.com>
2022-06-27 11:07:04 +0200

    add Write logic

--- playground/src/Write.tsx
+++ playground/src/Write.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import TextField from '@mui/material/TextField'
+import React, { useState } from 'react'
+import { useNfc } from 'nfc'
 
 const Write = () => {
-
+  const { isNDEFAvailable, permission, write } = useNfc()
+  const [value, setValue] = useState<string>('')
+  const handleWrite = async () => {
+    const re = await write(value)
+  }
   return (
-    <div
-      className='flex flex-col min-h-screen justify-start items-center p-4 md:p-8 bg-zinc-900 text-white'
+    <Container
+      sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', justifyContent: 'center', alignItems: 'center' }}
     >
-      WRITE PAGE
-    </div>
+      <TextField id="outlined-basic" label="Url" variant="outlined" value={value} onChange={(e) => {setValue(e.target.value)}}/>
+      <Button onClick={handleWrite}>Write</Button>
+    </Container>
   );
 }
 
