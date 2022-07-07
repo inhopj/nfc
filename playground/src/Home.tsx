24605eae82bf2ab116f99217c6b5ab7b3879447e
inhopj <fra.cirone@gmail.com>
2022-07-07 12:32:28 +0200

    add some info messages in Homepage for various scenarios

--- playground/src/Home.tsx
+++ playground/src/Home.tsx
@@ -4,32 +4,34 @@ import NfcIcon from '@mui/icons-material/Nfc';
 
 const Home = () => {
   const { isNDEFAvailable } = useNfc()
-  // const [mounted, setMounted] = useState(false)
-  // useEffect(() => {
-  //   setMounted(true)
-  // }, [])
   console.log("isNDEFAvailable: ", isNDEFAvailable);
   
   return (
     <div
-      className='flex flex-col min-h-screen justify-start items-center p-4 md:p-8'
+      className='flex flex-col min-h-screen justify-start items-center p-5 md:p-8'
     >
-      <div className='flex justify-start items-center p-4 md:p-8'>
-        <div>
+      <div className='flex justify-between items-center p-4 md:p-8'>
+        <div className=''>
           <NfcIcon />
         </div>
         <div>
-          Web-NFC PLAYGROUND
+          useNfc PLAYGROUND
         </div>
       </div>
 
-      {!isNDEFAvailable && (
+      {isNDEFAvailable !== undefined && !isNDEFAvailable && (
         <>
           Hey there! Looks like NFC is not available! <br />
           Please be sure to:<br />
           - Visit this page using a mobile phone {'('}NFC capable{')'}<br />
           - Use Chrome{'('}v89+{')'} or Opera{'('}v63+{')'}, more info at https://developer.mozilla.org/en-US/docs/Web/API/Web_NFC_API<br />
-          - Turn on NFC on your device - usually under Settings {'>'} Connections or Control Center
+          - Turn on NFC on your device. It's usually under Settings {'>'} Connections or Control Center
+        </>
+      )}
+      {isNDEFAvailable !== undefined && isNDEFAvailable && (
+        <>
+          Hey there! Your browser looks NFC capable, you're good to go!<br />
+          Try a Scan or a Write from one of the below tabs<br />
         </>
       )}
     </div>
