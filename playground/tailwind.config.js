b7ed18d3dd9f87f97388b11ad19bb62ee95e47c4
inhopj <fra.cirone@gmail.com>
2022-07-07 12:51:08 +0200

    add colors in tailwind config

--- playground/tailwind.config.js
+++ playground/tailwind.config.js
@@ -1,9 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      blue: colors.blue,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      zinc: colors.zinc
+    },
   },
   plugins: [],
 }
