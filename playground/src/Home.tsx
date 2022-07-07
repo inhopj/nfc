7d27a19c0dc2549918c46a0ab9b369bd157e896c
inhopj <fra.cirone@gmail.com>
2022-07-07 13:24:19 +0200

    add Alert for NFC not available scenario

--- playground/src/Home.tsx
+++ playground/src/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNfc } from 'nfc'
 import TapAndPlayIcon from '@mui/icons-material/TapAndPlay';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 const Home = () => {
   const { isNDEFAvailable } = useNfc()
@@ -13,19 +15,25 @@ const Home = () => {
       <div className='flex mb-4'>
         <TapAndPlayIcon color='primary' sx={{ fontSize: 70 }} />
       </div>
-      <div className='flex justify-center items-center w-full'>
-        <div className='flex text-xl text-gray-500'>
+      <div className='flex justify-center items-center w-full mb-10'>
+        <div className='flex text-xl text-gray-400'>
           useNfc Playground
         </div>
       </div>
 
       {isNDEFAvailable !== undefined && !isNDEFAvailable && (
         <>
-          Hey there! Looks like NFC is not available! <br />
-          Please be sure to:<br />
-          - Visit this page using a mobile phone {'('}NFC capable{')'}<br />
-          - Use Chrome{'('}v89+{')'} or Opera{'('}v63+{')'}, more info at https://developer.mozilla.org/en-US/docs/Web/API/Web_NFC_API<br />
-          - Turn on NFC on your device. It's usually under Settings {'>'} Connections or Control Center
+          <Alert severity="warning">
+            <AlertTitle>On Snap!</AlertTitle>
+            Looks like NFC is not available!<br />
+            Please be sure to:<br />
+            <strong>
+              - Visit this page using a mobile phone {'('}NFC capable{')'}<br />
+              - Use Chrome{'('}v89+{')'} or Opera{'('}v63+{')'}, more info at https://developer.mozilla.org/en-US/docs/Web/API/Web_NFC_API<br />
+              - Turn on NFC on your device. It's usually under Settings {'>'} Connections or Control Center
+            </strong>
+          </Alert>
+
         </>
       )}
       {isNDEFAvailable !== undefined && isNDEFAvailable && (
