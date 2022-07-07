c12c66ee9bb1170b04b45ea75042267db3f024eb
inhopj <fra.cirone@gmail.com>
2022-07-07 14:56:32 +0200

    add TappAndPlay iconwhen scannin in Scan page

--- playground/src/Scan.tsx
+++ playground/src/Scan.tsx
@@ -8,6 +8,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
+import TapAndPlayIcon from '@mui/icons-material/TapAndPlay';
 
 const Scan = () => {
   const { isNDEFAvailable, permission, read, abortReadCtrl, write } = useNfc()
@@ -20,40 +21,48 @@ const Scan = () => {
     try {
       setIsScanning(true)
       const re = await read()
-      
+
       const record = re.message.records[0]
-      
+
       console.log("RAW RECORD:", re.message.records[0]);
-      
+
       const decoder = new TextDecoder('utf-8');
-      
+
       const decodedContent = decoder.decode(record.data)
 
       console.log("DECODED RECORD:", decodedContent);
       setData(decodedContent)
       setOpen(true)
-      
+
     } catch (error) {
       console.log("ERROR ", error);
     }
   }
-  
+
   const handleClose = () => {
+    setIsScanning(false)
     setOpen(false)
     setData(null)
-    setIsScanning(false)
   }
 
   return (
     <Container
       sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', justifyContent: 'center', alignItems: 'center' }}
     >
-      <SensorsIcon sx={{ fontSize: 100 }}/>
       {!isScanning && <Button variant="contained" endIcon={<SensorsIcon />} onClick={handleRead}>
         Start Scan
       </Button>}
-      {isScanning && <div>Please tap the NFC tag with your device...</div>}
-      <Dialog
+      {isScanning && (
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex mb-4'>
+            <TapAndPlayIcon color='primary' sx={{ fontSize: 70 }} />
+          </div>
+          <div className='flex text-normal'>
+            Please tap the NFC tag with your device...
+          </div>
+        </div>
+      )}
+      {<Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
@@ -64,13 +73,13 @@ const Scan = () => {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            {data && data}
+            {data}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
         </DialogActions>
-      </Dialog>
+      </Dialog>}
     </Container>
   );
 }
