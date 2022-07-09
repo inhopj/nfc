1b7a3648d3b997b2f52e3836fdebdc49dbdd32a5
inhopj <fra.cirone@gmail.com>
2022-07-09 16:37:17 +0200

    playground - improve Write flow and UI

--- playground/src/Write.tsx
+++ playground/src/Write.tsx
@@ -10,6 +10,8 @@ import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Radio from '@mui/material/Radio'
 import FormHelperText from '@mui/material/FormHelperText'
+import TapAndPlayIcon from '@mui/icons-material/TapAndPlay';
+import Snackbar from '@mui/material/Snackbar'
 
 const Write = () => {
 
@@ -24,18 +26,45 @@ const Write = () => {
   const [isOk, setIsOk] = useState<boolean>(false)
   const [isError, setIsError] = useState<boolean>(false)
 
+  const isValidUrl = (value: string) => {
+    try {
+      const url = new URL(value)
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  const validation = (recordType: string, value: string) => {
+    if (recordType === '') {
+      setHelperText('Please select a Record Type')
+      return false
+    } else {
+      if (recordType === 'text') {
+        return true
+      } else if (recordType === 'url') {
+        const isValid = isValidUrl(value)
+        if ( value === '') {
+          setHelperText('Content for url Record Type can not be empty!')
+          return false
+        } else if (!isValid) {
+          setHelperText('Invalid url format!')
+          // return false
+          return true
+        } else {
+          return true
+        }
+      }
+    }
+  }
   const handleSubmit = async () => {
     try {
-      if (recordType === '') {
-        setHelperText('Please select a Record Type')
-      } else if (recordType === 'url' && data === '') {
-        setHelperText('Content for url Record Type can not be empty!')
-      } else {
+      if (validation(recordType, data)) {
         const record: NDEFRecordInit = { recordType: recordType, data: data as string }
         setIsScanning(true)
         const re = await write({ records: [record] })
         setIsOk(true)
-      }
+      } 
     } catch (error) {
       setIsError(true)
       console.log("ERROR ", error);
@@ -44,13 +73,31 @@ const Write = () => {
     }
   }
 
+  const handleCloseErrorSnackbar = () => {
+    setIsError(false);
+  };
+
   return (
     <Container
-      sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', justifyContent: 'center', alignItems: 'center' }}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        justifyContent: 'start',
+        alignItems: 'center',
+        paddingTop: 10
+      }}
     >
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        handleSubmit()
+      }}>
         <FormControl>
-          <FormLabel id="demo-controlled-radio-buttons-group">Record Type</FormLabel>
+          <FormLabel
+            id="demo-controlled-radio-buttons-group"
+          >
+            Record Type
+          </FormLabel>
           <RadioGroup
             aria-labelledby="demo-controlled-radio-buttons-group"
             name="controlled-radio-buttons-group"
@@ -59,7 +106,7 @@ const Write = () => {
               setHelperText('')
               setRecordType(e.target.value)
             }}
-            >
+          >
             <FormControlLabel value="text" control={<Radio />} label="Text" />
             <FormControlLabel value="url" control={<Radio />} label="Url" />
           </RadioGroup>
@@ -76,12 +123,18 @@ const Write = () => {
           />
         </FormControl>
         {!isScanning && (
-          <div>
-            <br></br>
-            <FormHelperText>{helperText}</FormHelperText>
+          <div className='flex flex-col mt-5'>
+            <FormHelperText
+              sx={{
+                display: 'flex',
+                marginBottom: 2
+              }}
+            >
+              {helperText}
+            </FormHelperText>
             <Button
               onClick={handleSubmit}
-              type="submit"
+              type="button"
               variant='contained'
             >
               Write
@@ -90,11 +143,22 @@ const Write = () => {
         )}
       </form>
       {isScanning && (
-        <div>Please tap the NFC tag with your device...</div>
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex mb-4'>
+            <TapAndPlayIcon color='primary' sx={{ fontSize: 70 }} />
+          </div>
+          <div className='flex text-normal'>
+            Please tap the NFC tag with your device...
+          </div>
+        </div>
       )}
       {isOk && <Alert severity="success">Content written successfully</Alert>}
 
-      {isError && <Alert severity="error">Something went wrong</Alert>}
+      <Snackbar open={isError} autoHideDuration={5000} onClose={handleCloseErrorSnackbar}>
+        <Alert onClose={handleCloseErrorSnackbar} severity="error" sx={{ width: '100%' }}>
+          Something went wrong!
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
